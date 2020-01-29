650a0824997e60f44565deaa3990668f36d2453d
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2020-01-29 11:56:50 -0300

    Usuário

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.send('Hello World');
-});
+routes.get('/users', UserController.index);
+routes.post('/users', UserController.store);
 
 export default routes;
