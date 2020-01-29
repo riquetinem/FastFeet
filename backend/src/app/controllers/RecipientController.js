e1c324f974c0739ae5db891fbc7c6a1b618e8999
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2020-01-29 13:59:39 -0300

    Controller destinatario

---
+++ backend/src/app/controllers/RecipientController.js
@@ -0,0 +1,11 @@
+class RecipientController {
+  async store(req, res) {
+    return res.json({ ok: true });
+  }
+
+  async update(req, res) {
+    return res.json({ ok: true });
+  }
+}
+
+export default new RecipientController();
