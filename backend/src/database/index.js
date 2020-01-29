257f3747bedc5e7e056523fa2204c682c2e646b8
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2020-01-29 13:59:45 -0300

    Criacao de rotas do destinatario

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,10 +1,13 @@
 import Sequelize from 'sequelize';
 
+// importacao das models
 import User from '../app/models/User';
+import Recipient from '../app/models/Recipient';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+// array com todos os models
+const models = [User, Recipient];
 
 class Database {
   constructor() {
@@ -14,6 +17,7 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
 
+    // para conectar a model (schema) no banco de dados
     models.map(model => model.init(this.connection));
   }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 // importacao dos controllers
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
 
 // importacao das middlewares
 import authMiddleware from './app/middlewares/auth';
@@ -21,5 +22,9 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 // rotas autenticadas
+
+// rotas de destinatario
+routes.post('/recipient', RecipientController.store);
+routes.put('/recipient', RecipientController.update);
 
 export default routes;
