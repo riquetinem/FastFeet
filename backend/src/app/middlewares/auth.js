5be1c82929b1796c345e9a7651f4cd92081ae4b0
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2020-01-29 13:19:13 -0300

    Middleware de autenticacao

---
+++ backend/src/app/middlewares/auth.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) return res.status(401).json({ error: 'Token not provided' });
+
+  // ele retorna um array de duas posicoes, precisamos pegar apenas a primeira
+  // por isso o uso do [, ] o primeiro parametro pode ser apenas ignorado
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
+};
