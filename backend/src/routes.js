22b173207c15c9ff25079138b87f22b2edc8ba9d
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2020-01-29 13:18:52 -0300

    Criacao do token

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,25 @@
 import { Router } from 'express';
 
+// importacao dos controllers
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+// importacao das middlewares
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
+// rotas sem autenticacao
+
 // rotas de usuario
 routes.post('/users', UserController.store);
+
+// rota para criar uma sessao
+routes.post('/sessions', SessionController.store);
+
+// setando a middleware para as rotas depois dela
+routes.use(authMiddleware);
+
+// rotas autenticadas
 
 export default routes;
