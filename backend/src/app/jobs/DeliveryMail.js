e11a2d9c21da3edb147123dbea0ba624a9d32ff0
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2020-01-30 19:56:37 -0300

    Configurando o email para utilizar o template do hbs

---
+++ backend/src/app/jobs/DeliveryMail.js
@@ -0,0 +1,24 @@
+import MailConfig from '../../lib/Mail';
+
+class DeliveryMail {
+  get key() {
+    return 'DeliveryMail';
+  }
+
+  async handle({ data }) {
+    const { delivery } = data;
+
+    await MailConfig.sendMail({
+      to: `${delivery.deliveryman.name} <${delivery.deliveryman.email}>`,
+      subject: 'Nova entrega',
+      template: 'sendDelivery',
+      context: {
+        deliveryman: delivery.deliveryman.name,
+        recipient: delivery.recipient.name,
+        product: delivery.product,
+      },
+    });
+  }
+}
+
+export default new DeliveryMail();
