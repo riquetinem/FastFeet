e3e49da6b578cda6dc6f919a8cd03352354aa208
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2020-02-25 06:53:17 -0300

    Adicionando a rota de entregador

--- frontend/src/store/modules/rootReducer.js
+++ frontend/src/store/modules/rootReducer.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import auth from './auth/reducer';
 import user from './user/reducer';
 import deliveries from './deliveries/reducer';
+import deliverymen from './deliverymen/reducer';
 
 export default combineReducers({
   auth,
   user,
   deliveries,
+  deliverymen,
 });
