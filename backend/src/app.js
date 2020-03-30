d4d6601864543bc2b4387c7050ae875b7438e33f
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2020-03-30 16:37:00 -0300

    Segurança no node

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,10 @@ import 'dotenv/config';
 
 import express from 'express';
 import path from 'path';
+import helmet from 'helmet';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
 import cors from 'cors';
 import Youch from 'youch';
 
@@ -20,13 +24,29 @@ class App {
 
   midlewares() {
     this.server.use(express.json());
-
+    this.server.use(helmet());
     this.server.use(cors());
+
     // passando o path de onde vao os arquivos upados
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
     );
+
+    if (process.env.NODE_ENV !== 'development') {
+      this.server.use(
+        new RateLimit({
+          store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            }),
+          }),
+          windowMs: 1000 * 60 * 15,
+          max: 100,
+        })
+      );
+    }
   }
 
   routes() {
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import Brute from 'express-brute';
+import BruteRedis from 'express-brute-redis';
 import multer from 'multer';
 
 import multerConfig from './config/multer';
@@ -30,13 +32,25 @@ import authMiddleware from './app/middlewares/auth';
 const routes = new Router();
 const upload = multer(multerConfig);
 
+const bruteStore = new BruteRedis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+});
+
+const bruteForce = new Brute(bruteStore);
+
 // rotas sem autenticacao
 
 // rotas de usuario
 routes.post('/users', validateUserStore, UserController.store);
 
 // rota para criar uma sessao
-routes.post('/sessions', validateSessionStore, SessionController.store);
+routes.post(
+  '/sessions',
+  bruteForce.prevent,
+  validateSessionStore,
+  SessionController.store
+);
 
 // mostrar horarios disponiveis
 routes.get('/deliveryman/available', AvailableController.index);
