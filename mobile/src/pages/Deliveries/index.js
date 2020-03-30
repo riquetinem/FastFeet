cf71fa0f0627ea0d879255cee3010221cc3792b0
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2020-03-30 15:24:21 -0300

    Ajustando algumas coisas (logout e listagem)

--- mobile/src/pages/Deliveries/index.js
+++ mobile/src/pages/Deliveries/index.js
@@ -72,6 +72,22 @@ export default function Deliveries() {
         const data = res.data.map(response => ({
           ...response,
           idFormated: `00${response.id}`.slice(-2),
+          endereco: `${response.recipient.rua}, ${response.recipient.numero}, ${response.recipient.cidade} - ${response.recipient.estado}, ${response.recipient.cep}`,
+          retirada:
+            response.start_date &&
+            format(parseISO(response.start_date), 'dd/MM/yyyy', {
+              locale: pt,
+            }),
+          entregue:
+            response.end_date &&
+            format(parseISO(response.end_date), 'dd/MM/yyyy', {
+              locale: pt,
+            }),
+          status: response.end_date
+            ? 'Entregue'
+            : response.start_date
+            ? 'Retirada'
+            : 'Pendente',
         }));
 
         setDeliveries(data);
@@ -79,7 +95,8 @@ export default function Deliveries() {
     }
 
     loadDeliveries();
-  }, [menuSelected, user.id]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [menuSelected]);
 
   function handleLogout() {
     dispatch(signOut());
@@ -91,14 +108,16 @@ export default function Deliveries() {
         <User>
           <Avatar
             source={{
-              uri: user.avatar
-                ? user.avatar.url
-                : `https://api.adorable.io/avatars/285/${user.name}.png`,
+              uri:
+                user && user.avatar
+                  ? user.avatar.url
+                  : `https://api.adorable.io/avatars/285/${user &&
+                      user.name}.png`,
             }}
           />
           <Welcome>
             <Message>Bem vindo de volta,</Message>
-            <Title>{user.name}</Title>
+            <Title>{user && user.name}</Title>
           </Welcome>
         </User>
 
--- mobile/src/pages/Profile/index.js
+++ mobile/src/pages/Profile/index.js
@@ -13,7 +13,6 @@ import {
 import { signOut } from '~/store/modules/auth/actions';
 
 export default function Profile() {
-  // TODO: Verificar o pq quando dar logout ele "redireciona" para a mesma pagina e fala que o "user.blabla is null"
   const user = useSelector(state => state.user.profile);
 
   const dispatch = useDispatch();
@@ -26,21 +25,22 @@ export default function Profile() {
     <Container>
       <Avatar
         source={{
-          uri: user.avatar
-            ? user.avatar.url
-            : `https://api.adorable.io/avatars/285/${user.name}.png`,
+          uri:
+            user && user.avatar
+              ? user.avatar.url
+              : `https://api.adorable.io/avatars/285/${user && user.name}.png`,
         }}
       />
 
       <Info>
         <Title>Nome Completo</Title>
-        <ContentInfo>{user.name}</ContentInfo>
+        <ContentInfo>{user && user.name}</ContentInfo>
 
         <Title>Email</Title>
-        <ContentInfo>{user.email}</ContentInfo>
+        <ContentInfo>{user && user.email}</ContentInfo>
 
         <Title>Data de cadastro</Title>
-        <ContentInfo>{user.createdAt}</ContentInfo>
+        <ContentInfo>{user && user.createdAt}</ContentInfo>
       </Info>
 
       <LogoutButton onPress={handleLogout}>Logout</LogoutButton>
